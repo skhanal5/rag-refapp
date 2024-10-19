1c2bf9154f15e22a6cc869c17bf8d759747dea12
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-19 18:46:42 -0400

    Add implementation for ingest route

--- rag_refapp/api/routes/ingest.py
+++ rag_refapp/api/routes/ingest.py
@@ -1,25 +1,25 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from rag_refapp.api.opensearch.database_config import DatabaseConfig
 from rag_refapp.api.opensearch.vector_database import VectorDatabase
 
 router = APIRouter(prefix="/ingest", tags=["ingest"])
 config = DatabaseConfig(
-    hostname="0.0.0.0",
+    hostname="localhost",
     port=9200,
     auth=("admin", "@ThisIsMyPassword123"),
-    ssl_flag=False,
+    ssl_flag=True,
     verify_cert_flag=False,
     ssl_show_warn=False,
 )
 database = VectorDatabase(config)
 
 
-@router.post("/")
-async def setup_index(index_name: str):
-    return database.create_index(index_name)
+class SetupDetails(BaseModel):
+    index_name: str
 
 
-@router.post("/")
-async def ingest_document(index_name: str):
-    return database.add_document(index_name, "")
+@router.post("/setup/")
+async def setup_index(request_body: SetupDetails):
+    return database.create_index(request_body.index_name)
