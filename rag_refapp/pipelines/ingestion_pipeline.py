549f79ac89a313b96b7588b5263c6e0c6b996eb3
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-20 17:46:59 -0400

    Expand ingestion pipeline to include components for cleaning and chunking

--- rag_refapp/pipelines/ingestion_pipeline.py
+++ rag_refapp/pipelines/ingestion_pipeline.py
@@ -1,32 +1,56 @@
-from haystack import Pipeline, Document
+from pathlib import Path
+
+from haystack import Pipeline
+from haystack.components.converters import TextFileToDocument
 from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.preprocessors import DocumentCleaner, DocumentSplitter
 from haystack.components.writers import DocumentWriter
+from haystack.utils import Secret
 from haystack_integrations.document_stores.opensearch import (
     OpenSearchDocumentStore,
 )
 
 
-class EmbeddingPipeline:
+class IngestionPipeline:
 
     def __init__(self):
         self.pipeline = Pipeline()
 
-    def execute_pipeline(self, documents: list[Document]):
+    def execute_pipeline(self, path: str):
+
+        # TODO: works on markdown only
+        files = list(Path(path).glob("*.md"))
 
         # Init dependencies for components
-        document_writer = EmbeddingPipeline.__setup_writer()
+        document_writer = IngestionPipeline.__setup_writer()
 
-        # Add all components for this pipeline
+        # Add all components for this
         self.pipeline.add_component(
-            "text_embedder", SentenceTransformersTextEmbedder(model="model")
+            instance=TextFileToDocument(), name="text_file_converter"
+        )
+        self.pipeline.add_component(instance=DocumentCleaner(), name="cleaner")
+        self.pipeline.add_component(
+            instance=DocumentSplitter(split_by="sentence", split_length=1),
+            name="splitter",
+        )
+        self.pipeline.add_component(
+            "text_embedder",
+            SentenceTransformersTextEmbedder(
+                token=Secret.from_token("<your-api-key>")
+            ),  # Uses default model
         )
         self.pipeline.add_component(instance=document_writer, name="writer")
 
         # Connect the components together
+        self.pipeline.connect(
+            "text_file_converter.documents", "cleaner.documents"
+        )
+        self.pipeline.connect("cleaner.documents", "splitter.documents")
+        self.pipeline.connect("splitter.documents", "writer.documents")
         self.pipeline.connect("text_embedder", "writer")
 
         # Execute the pipeline
-        self.pipeline.run({"embedder": {"documents": documents}})
+        self.pipeline.run({"embedder": {"documents": files}})
 
     @staticmethod
     def __setup_writer():
@@ -34,7 +58,7 @@ class EmbeddingPipeline:
             hosts="http://localhost:9200",
             use_ssl=True,
             verify_certs=False,
-            http_auth=("admin", "admin"),
+            http_auth=("admin", "@ThisIsMyPassword123"),
         )
         document_writer = DocumentWriter(document_store=document_store)
         return document_writer
