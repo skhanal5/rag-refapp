45f6e455a6bf78cfad38bf09baac4834ccc11fcc
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-20 17:00:54 -0400

    Add rag pipeline definition

---
+++ rag_refapp/pipelines/rag_pipeline.py
@@ -0,0 +1,69 @@
+from haystack import Pipeline
+from haystack.components.builders import PromptBuilder
+from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.generators import HuggingFaceLocalGenerator
+from haystack.utils import Secret
+from haystack_integrations.components.retrievers.opensearch import (
+    OpenSearchEmbeddingRetriever,
+)
+from haystack_integrations.document_stores.opensearch import (
+    OpenSearchDocumentStore,
+)
+
+
+class RAGPipeline:
+
+    pipeline: Pipeline
+
+    # TODO: is this pythonic?
+    def __init__(self):
+        self.pipeline = Pipeline()
+
+    def execute_pipeline(self, query: str):
+        template = """
+        Given the following information, answer the question.
+
+        Context:
+        {% for document in documents %}
+            {{ document.content }}
+        {% endfor %}
+
+        Question: {{question}}
+        Answer:
+        """
+
+        document_store = OpenSearchDocumentStore(
+            hosts="http://localhost:9200",
+            use_ssl=True,
+            verify_certs=False,
+            http_auth=("admin", "admin"),
+        )
+
+        prompt_builder = PromptBuilder(template=template)
+        generator = HuggingFaceLocalGenerator(
+            token=Secret.from_token("<your-api-key>")
+        )
+        generator.warm_up()
+
+        # Add all components for this pipeline
+        self.pipeline.add_component(
+            "text_embedder", SentenceTransformersTextEmbedder(model="model")
+        )
+        self.pipeline.add_component(
+            "retriever",
+            OpenSearchEmbeddingRetriever(document_store=document_store),
+        )
+        self.pipeline.add_component("prompt_builder", prompt_builder)
+        self.pipeline.add_component("llm", generator)
+
+        # Connect the components together
+        self.pipeline.connect(
+            "text_embedder.embedding", "retriever.query_embedding"
+        )
+        self.pipeline.connect("retriever", "prompt_builder.documents")
+        self.pipeline.connect("prompt_builder", "llm")
+
+        # Execute the pipeline
+        self.pipeline.run(
+            {"prompt_builder": {"query": query}, "retriever": {"query": query}}
+        )
