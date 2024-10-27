338d95334883e62d7d6aed106234e5bba1ceec61
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-27 16:56:57 -0400

    Fix bug with type hints in opensearch client, add type hints to open search config, update dependency access

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,6 +3,7 @@ from functools import lru_cache
 from app import config
 from app.opensearch.database import OpenSearchClient
 from app.opensearch.database_config import OpenSearchConfig
+from app.services.retrieval_service import RetrievalService
 
 """
     This module serves as a singleton. This is Pythonic...?
@@ -15,9 +16,15 @@ def get_settings():
 
 
 @lru_cache
-def _get_opensearch_config():
+def get_opensearch_config():
     return OpenSearchConfig(get_settings())
 
 
+@lru_cache
 def get_opensearch_client():
-    return OpenSearchClient(_get_opensearch_config())
+    return OpenSearchClient(get_opensearch_config())
+
+
+@lru_cache
+def get_retrieval_service():
+    return RetrievalService(get_settings())
--- app/opensearch/database_config.py
+++ app/opensearch/database_config.py
@@ -4,10 +4,10 @@ from app.config import Settings
 class OpenSearchConfig:
 
     def __init__(self, settings: Settings):
-        self.hostname = settings.opensearch_hostname
-        self.port = settings.opensearch_port
-        self.auth = {
+        self.hostname: str = settings.opensearch_hostname
+        self.port: int = settings.opensearch_port
+        self.auth: dict[str, str] = {
             "username": settings.opensearch_username,
             "password": settings.opensearch_password,
         }
-        self.ssl_flag = settings.opensearch_ssl_flag
+        self.ssl_flag: bool = settings.opensearch_ssl_flag
