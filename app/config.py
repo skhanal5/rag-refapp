68985817ff89d386f84f2271edb7e2e608ff867a
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-26 21:20:03 -0400

    Move OpenSearch package out of api, update config with opensearch env vars

--- app/config.py
+++ app/config.py
@@ -7,5 +7,10 @@ class Settings(BaseModel):
     embedding_model: str
     reranking_model: str
     text_generation_model: str
+    opensearch_hostname = str
+    opensearch_port = int
+    opensearch_username = int
+    opensearch_password = int
+    opensearch_ssl_flag = bool
 
     model_config = SettingsConfigDict(env_file=".env")
---
+++ app/opensearch/database_config.py
@@ -0,0 +1,14 @@
+from app.config import Settings
+
+
+class OpenSearchConfig:
+
+    # turn to factory method
+    def __init__(self, settings: Settings):
+        self.hostname = settings.opensearch_hostname
+        self.port = settings.opensearch_port
+        self.auth = {
+            "username": settings.opensearch_username,
+            "password": settings.opensearch_password,
+        }
+        self.ssl_flag = settings.opensearch_ssl_flag
