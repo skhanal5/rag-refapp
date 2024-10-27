ee20e61df924adc37091ee510e8fe62505feb89a
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-27 14:42:40 -0400

    Update index to make use of client dependency

--- app/api/routes/index.py
+++ app/api/routes/index.py
@@ -1,19 +1,12 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
-from app.api.opensearch.database_config import DatabaseConfig
-from app.api.opensearch.vector_database import VectorDatabase
+from app.dependencies import get_opensearch_client
+from app.opensearch.database import OpenSearchClient
 
 router = APIRouter(prefix="/index", tags=["index"])
-config = DatabaseConfig(
-    hostname="localhost",
-    port=9200,
-    auth=("admin", "@ThisIsMyPassword123"),
-    ssl_flag=True,
-    verify_cert_flag=False,
-    ssl_show_warn=False,
-)
-database = VectorDatabase(config)
 
 
 class SetupDetails(BaseModel):
@@ -29,22 +22,34 @@ class Document(BaseModel):
 
 
 @router.post("/")
-async def add_index(request_body: SetupDetails):
-    return database.create_index(request_body.index_name)
+async def add_index(
+    request_body: SetupDetails,
+    client: Annotated[OpenSearchClient, Depends(get_opensearch_client)],
+):
+    return client.create_index(request_body.index_name)
 
 
 @router.delete("/")
-async def delete_index(request_body: SetupDetails):
-    return database.delete_index(request_body.index_name)
+async def delete_index(
+    request_body: SetupDetails,
+    client: Annotated[OpenSearchClient, Depends(get_opensearch_client)],
+):
+    return client.delete_index(request_body.index_name)
 
 
 @router.post("/{index_name}/document")
-async def add_document(index_name: str, request_body: Document):
-    return database.add_document(
-        index_name, {"contents": request_body.contents}
-    )
+async def add_document(
+    index_name: str,
+    request_body: Document,
+    client: Annotated[OpenSearchClient, Depends(get_opensearch_client)],
+):
+    return client.add_document(index_name, {"contents": request_body.contents})
 
 
 @router.delete("/{index_name}/document")
-async def delete_document(index_name: str, request_body: DocumentDetails):
-    return database.delete_document(index_name, request_body.doc_id)
+async def delete_document(
+    index_name: str,
+    request_body: DocumentDetails,
+    client: Annotated[OpenSearchClient, Depends(get_opensearch_client)],
+):
+    return client.delete_document(index_name, request_body.doc_id)
